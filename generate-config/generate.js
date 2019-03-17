ef4f30826fbe32ee401e5fbcf46c6b29cf9b11b1
hardik <hardik_satasiya@yahoo.com>
2019-03-17 22:49:00 +0530

    dynamic serice generate algo.

--- generate-config/generate.js
+++ generate-config/generate.js
@@ -4,7 +4,22 @@ var fs = require('fs');
 const args = process.argv.slice(2)
 
 // writeFile function with filename, content and callback function
-fs.writeFile('config.yaml', `Generated Using NODE.JS ${args[0]}`, function (err) {
+fs.writeFile('service-config.yaml', `apiVersion: v1
+kind: Service
+metadata:
+  labels:
+    run: nginx
+  name: nginx-${args[0]}
+  namespace: default
+spec:
+  ports:
+  - port: 80
+    protocol: TCP
+    targetPort: 80
+  selector:
+    run: nginx
+  type: NodePort
+`, function (err) {
   if (err) throw err;
   console.log('File is created successfully.');
 });
