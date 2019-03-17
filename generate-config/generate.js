8a9fca71ea0dddc2ce7140ba3285f717831dd7e8
hardik <hardik_satasiya@yahoo.com>
2019-03-17 22:35:03 +0530

    same

--- generate-config/generate.js
+++ generate-config/generate.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 const args = process.argv.slice(2)
 
 // writeFile function with filename, content and callback function
-fs.writeFile('config.yaml', 'Generated Using NODE.JS ${args[0]}', function (err) {
+fs.writeFile('config.yaml', `Generated Using NODE.JS ${args[0]}`, function (err) {
   if (err) throw err;
   console.log('File is created successfully.');
 });
