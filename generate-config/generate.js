ec8601bc2c159c21473256bdff9f54ac44579fdd
hardik <hardik_satasiya@yahoo.com>
2019-03-18 14:10:58 +0530

    same

--- generate-config/generate.js
+++ generate-config/generate.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 const args = process.argv.slice(2)
 
 // writeFile function with filename, content and callback function
+
 fs.writeFile('service-config.yaml', `apiVersion: v1
 kind: Service
 metadata:
