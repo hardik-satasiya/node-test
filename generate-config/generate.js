9a439772ef07293318217f5b93160307bf05278e
hardik <hardik_satasiya@yahoo.com>
2019-03-21 15:48:29 +0530

    auto deploy test

--- generate-config/generate.js
+++ generate-config/generate.js
@@ -1,12 +1,18 @@
 // include node fs module
+// npm run generate-config $APP $GIT_COMMIT $GIT_BRANCH
 var fs = require('fs');
 
 const args = process.argv.slice(2)
 
-// writeFile function with filename, content and callback function
+const APP = args[0]
+const GIT_COMMIT = args[1]
+const GIT_BRANCH = args[2]
 
+const K8S_CLUSTER_URL = APP + '-' + GIT_BRANCH + '.fashionfortret.com'
+const DockerRegistryUrl = 'gcr.io/advance-verve-234809/github.com/hardik-satasiya/node-test'
 
-fs.writeFile('service-config.yaml', `apiVersion: v1
+/*
+apiVersion: v1
 kind: Service
 metadata:
   labels:
@@ -21,7 +27,68 @@ spec:
   selector:
     run: nginx
   type: NodePort
+*/
+
+// writeFile function with filename, content and callback function
+fs.writeFile('deployment-config.yaml', `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: ${APP}-deployment
+  labels:
+    app: ${APP}
+    version: ${GIT_COMMIT}
+    pull-request: ${GIT_BRANCH}
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: ${APP}
+  template:
+    metadata:
+      labels:
+        app: ${APP}
+    spec:
+      containers:
+      - name: wegde-ibe
+        image: ${DockerRegistryUrl}:${GIT_COMMIT}
+        ports:
+        - containerPort: 3000
+
+---
+
+apiVersion: v1
+kind: Service
+metadata:
+  name: ${APP}-service
+spec:
+  selector:
+    app: ${APP}
+  ports:
+  - protocol: TCP
+    name: http
+    port: 80
+    targetPort: 3000
+  - protocol: TCP
+    name: https
+    port: 443
+    targetPort: 3000
+
+---
+
+apiVersion: extensions/v1beta1
+kind: Ingress
+metadata:
+  name: ${APP}-ingress
+spec:
+  rules:
+  - host: ${K8S_CLUSTER_URL}
+    http:
+      paths:
+        - backend:
+            serviceName: ${APP}-service
+            servicePort: 80
 `, function (err) {
   if (err) throw err;
-  console.log('File is created successfully.');
+  console.log('Deploy Config File is created successfully.');
 });
+
